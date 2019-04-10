d3189bd1f3d3b06222997c39ea686ff7ba560df1
ricardobah <rickribeiro99@gmail.com>
2019-04-10 16:35:18 -0300

    ajustando com o netn]inja course

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
-const cors = require("cors")
+
 const app = express();
 app.use(express.json());
-app.use(cors()) // Estudar cors
+app.set('view engine', 'ejs')
 const requireDir = require('require-dir');
 // requireDir('./src/models')
 app.use('/',require("./src/routes"))
 
-app.listen('3001');
+app.get('/', (req,res) =>{
+    res.render('index')
+})
+
+app.listen('3000', () => {
+    console.log('app now')
+});
