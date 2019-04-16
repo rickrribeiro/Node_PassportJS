7ef144b2e97ef04220dcc6d380225493ea2c9267
ricardobah <rickribeiro99@gmail.com>
2019-04-16 13:19:34 -0300

    profile

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
-const cookieSession = require(cookie-session)
+const cookieSession = require('cookie-session')
 const app = express();
 const passport= require('passport')
 
@@ -27,7 +28,7 @@ mongoose.connect(keys.mongodb.dbURI, ()=>{
 
 // set up routes
 app.use('/auth', authRoutes);
-
+app.use('/profile', profileRoutes);
 // create home route
 app.get('/', (req, res) => {
     res.render('home');
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -19,7 +19,8 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'),(req, res) => {
-    res.send('you reached the redirect URI');
+    res.redirect('/profile/')
+    
 });
 
 module.exports = router;
